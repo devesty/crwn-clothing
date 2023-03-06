8b3f8c31de6105dee725dbf400c268ca7463bce5
devesty <estherharriet2@gmail.com>
2023-03-06 13:26:14 +0100

    Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './App.css';
 
+import { Routes, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component.jsx';
+
+const HatsPage = () => (
+  <div>
+    <h1>HATS PAGE</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage/>
+      <Routes>
+      <Route exact path='/' element={<HomePage/>}/>
+      <Route path='/hats' element={<HatsPage/>}/>
+      </Routes>
+     
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -2,16 +2,40 @@ import React from "react";
 
 import './menu-item.styles.scss';
 
+import {useNavigate} from 'react-router-dom';
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className= {`${size} menu-item`}>
-        <div className='background-image' style= {{
-            backgroundImage: `url(${imageUrl})`
-        }}/>
-    <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">SHOP NOW</span>
-    </div>
-</div>
-)
-export default MenuItem;
+
+// const MenuItem = ({title, imageUrl, size, history, linkUrl, match}) => (
+
+//     <div className= {`${size} menu-item`} onClick={() => history.push(`${match.url}${linkUrl}`)}>
+//         <div className='background-image' style= {{
+//             backgroundImage: `url(${imageUrl})`
+//         }}/>
+//     <div className="content">
+//         <h1 className="title">{title.toUpperCase()}</h1>
+//         <span className="subtitle">SHOP NOW</span>
+//     </div>
+// </div>
+// )
+// export default (MenuItem);
+
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => {
+    let navigate = useNavigate();
+    return (
+      <div className={`${size} menu-item`} onClick={() => navigate(`${linkUrl}`)}>
+        <div
+          className="background-image"
+          style={{
+            backgroundImage: `url(${imageUrl})`,
+          }}
+        ></div>
+  
+        <div className="content">
+          <h1 className="title">{title.toUpperCase()}</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    );
+  };
+  
+  export default MenuItem;
